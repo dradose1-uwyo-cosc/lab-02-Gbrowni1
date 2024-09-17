f4087f75c0c2a6c6e0ac8b993be238c3dc9b30ee
GRANIT <gbrowni1@uwyo.edu>
2024-09-17 22:04:01 +0000

    Literally an

--- cosc1010-lab02.py
+++ cosc1010-lab02.py
@@ -1,41 +1,46 @@
-# Your Name Here
+# Grant Browning
 # UWYO COSC 1010
-# Submission Date
-# Lab 02 
-# Lab Section: 
-# Sources, people worked with, help given to: 
-# your
-# comments
-# here
+# 09/17/24
+# Lab 02
+# Lab Section: 10  
+# Sources: Google - remembering how to make a title case.
+# People worked with: Ben Mathis
 
-your_variable_here = "when you see this, replace it with your code"
+#your_variable_here = "when you see this, replace it with your code"
 
 ## Section ONE
 
 # Complete the following print statement to print out "Hello, COSC1010"
-print()
+
+print("Hello, COSC1010")
 
 # Assign the string above to a variable named hello_message and print that variable
-hello_message = ""
-print("Your code here")
+hello_message = "Hello, COSC1010"
+print(hello_message)
 
 # Assign the string "cowboy joe" to a variable, and print that variable with title casing
-print("Your code here")
+gopokes = "cowboy joe"
+print(gopokes.title())
 
 # Complete the following f-string print message 
     # You will need to create your own variables and insert them  
     # the final message should read `The University of Wyoming was founded in 1886`
-
-print(f"The {your_variable_here} was founded in {your_variable_here}")
+V_school = "University of Wyoming"
+V_date = "1886"
+print(f"The {V_school} was founded in {V_date}")
 
 # Now let's do some math with variables 
     # Create two variables x and y and assign them the values 5 and 10 respectively 
     # Complete the following print statements using your variables
     #All math must be done within the braces in the f-strings
-print(f"x + y = {your_variable_here}")
-print(f"x - y = {your_variable_here}")
-print(f"x * y = {your_variable_here}")
-print(f"x / y = {your_variable_here}")
+ 
+x = 5
+y = 10
+
+print(f"x + y = {x+y}")
+print(f"x - y = {x-y}")
+print(f"x * y = {x*y}")
+print(f"x / y = {x/y}")
 
 # String concatenation 
     # Finally we will take a look at string concatenation
@@ -46,3 +51,8 @@ print(f"x / y = {your_variable_here}")
         # last_name, which is your last name
         # space, which is a space character 
     # Use string concatenation to print out your full name 
+
+first_name = "Grant"
+last_name = "Browning"
+space = " "
+print(f"{first_name}{space}{last_name}")
